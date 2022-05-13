db5e07c836a4ce0ad0597ef698792371f98133a3
lou <test@email.com>
2022-05-13 17:29:34 +0800

    comit3

--- src/TestGitPrint1.java
+++ src/TestGitPrint1.java
@@ -3,7 +3,10 @@
  * @create 2022-05-13
  */
 public class TestGitPrint1 {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("git test1");
+        System.out.println("git test1");
         System.out.println("git test1");
     }
 }
